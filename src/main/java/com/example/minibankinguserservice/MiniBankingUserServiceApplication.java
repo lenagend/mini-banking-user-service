878a843dcca6714513cbac6f4eb115b514bc775f
lenagend <lenagend@naver.com>
2024-03-20 16:27:30 +0900

    Initial commit

---
+++ src/main/java/com/example/minibankinguserservice/MiniBankingUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.minibankinguserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBankingUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniBankingUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/minibankinguserservice/MiniBankingUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minibankinguserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBankingUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
