290ff66e5fddfa1461622dfe2f4c8e9708774caf
lenagend <lenagend@naver.com>
2024-03-21 17:37:57 +0900

    feat(UserController,UserService, application.properties): UserController,UserService 추가 및 application.properties 설정코드 추가
    UserService 사용자 관련 비즈니스 로직을 처리하는 서비스레이어. 사용자 생성, 조회 등
    UserController 사용자 관련 요청을 처리하는 컨트롤러. RESTful API를 통해 외부에서 UserService 대한 요청을 중개
    application.properties 서버 포트 설정 8081
    
    기존 코드에 영향을 주지 않습니다.
    
    Closes X

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        return user != null ? ResponseEntity.ok(user) : ResponseEntity.notFound().build();
+    }
+
+    // 기타 엔드포인트 구현
+}
---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,25 @@
+package service;
+
+import entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        // 사용자 생성 로직
+        return userRepository.save(user);
+    }
+
+    public User getUserById(Long id) {
+        // ID로 사용자 조회 로직
+        return userRepository.findById(id).orElse(null);
+    }
+
+
+}
